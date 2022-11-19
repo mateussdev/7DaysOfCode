a2745b040ffa3821af3d743a4c62ff9e4cb97340
mateussdev <mateusbs427@gmail.com>
2022-11-19 16:14:02 -0300

    :ballot_box_with_check: Day 4

--- logic-javascript/day-4/index.js
+++ logic-javascript/day-4/index.js
@@ -1,32 +1,31 @@
 function game() {
   const number = Math.floor(Math.random() * (10 - 0 + 1) + 0);
 
-const guess = prompt(
-  "Pensei em um número entre 0 e 10. Dê o seu palpite de que número é esse!"
-);
-
-if (guess == number) {
-  alert("Parabéns!!! Você acertou!");
-} else {
-  alert(
-    "Que pena, você errou!!! Você só tem mais duas tentativas, use com sabedoria!"
+  const guess = prompt(
+    "Pensei em um número entre 0 e 10. Dê o seu palpite de que número é esse!"
   );
 
-  let i = 1;
-  while (i < 3) {
-    const guessExtra = prompt("Tente outra vez...");
-    if (guessExtra == number) {
-      alert("Parabéns, você acertou!!!");
-      i = 4;
-    }
-    i++;
-    if (guessExtra != number && i >= 3) {
-      alert(`Infelizmente você não acertou, o númera era ${number}`);
-      i = 4;
+  if (guess == number) {
+    alert("Parabéns!!! Você acertou!");
+  } else {
+    alert(
+      "Que pena, você errou!!! Você só tem mais duas tentativas, use com sabedoria!"
+    );
+
+    let i = 1;
+    while (i < 3) {
+      const guessExtra = prompt("Tente outra vez...");
+      if (guessExtra == number) {
+        alert("Parabéns, você acertou!!!");
+        i = 4;
+      }
+      i++;
+      if (guessExtra != number && i >= 3) {
+        alert(`Infelizmente você não acertou, o númera era ${number}`);
+        i = 4;
+      }
     }
   }
-
-}
 }
 
 game();
